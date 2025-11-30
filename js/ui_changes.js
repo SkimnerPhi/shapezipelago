cad8a4d1b37436a9c4cd4037f18f38f1ffa2cbf5
Skimner Phi <skimnerphi@gmail.com>
2025-11-29 19:19:46 -0500

    Rewrite main menu patch

--- js/ui_changes.js
+++ js/ui_changes.js
@@ -6,98 +6,82 @@ import { T } from "shapez/translations";
 
 export function addInputContainer(modImpl, root) {
     logger.debug("Calling addInputContainer");
+
     modImpl.signals.stateEntered.add((state) => {
-        if (state.key === "MainMenuState") {
-            logger.debug("Creating input box");
-            // add input box
-            const mainWrapper = document.body.getElementsByClassName("mainWrapper").item(0);
-            const sideContainer = mainWrapper.getElementsByClassName("sideContainer").item(0);
-            const inputContainer = document.createElement("div");
-            inputContainer.className = "inputContainer";
-            sideContainer.appendChild(inputContainer);
-            const playerContainer = document.createElement("div");
-            const addressContainer = document.createElement("div");
-            const portContainer = document.createElement("div");
-            const passwordContainer = document.createElement("div");
-            const statusContainer = document.createElement("div");
-            playerContainer.className = "playerContainer";
-            addressContainer.className = "addressContainer";
-            portContainer.className = "portContainer";
-            passwordContainer.className = "passwordContainer";
-            statusContainer.className = "statusContainer";
-            inputContainer.appendChild(playerContainer);
-            inputContainer.appendChild(addressContainer);
-            inputContainer.appendChild(portContainer);
-            inputContainer.appendChild(passwordContainer);
-            inputContainer.appendChild(statusContainer);
-            const playerLabel = document.createElement("h4");
-            const playerInput = document.createElement("input");
-            playerLabel.innerText = T.mods.shapezipelago.inputBox.player;
-            playerInput.type = "text";
-            playerInput.className = "playerInput";
-            playerInput.value = modImpl.settings.player;
-            playerContainer.appendChild(playerLabel);
-            playerContainer.appendChild(playerInput);
-            const addressLabel = document.createElement("h4");
-            const addressInput = document.createElement("input");
-            addressLabel.innerText = T.mods.shapezipelago.inputBox.address;
-            addressInput.type = "text";
-            addressInput.className = "addressInput";
-            addressInput.value = modImpl.settings.address;
-            addressContainer.appendChild(addressLabel);
-            addressContainer.appendChild(addressInput);
-            const portLabel = document.createElement("h4");
-            const portInput = document.createElement("input");
-            portLabel.innerText = T.mods.shapezipelago.inputBox.port;
-            portInput.type = "number";
-            portInput.value = modImpl.settings.port;
-            portContainer.appendChild(portLabel);
-            portContainer.appendChild(portInput);
-            const passwordLabel = document.createElement("h4");
-            const passwordInput = document.createElement("input");
-            passwordLabel.innerText = T.mods.shapezipelago.inputBox.password;
-            passwordInput.type = "password";
-            passwordInput.value = modImpl.settings.password;
-            passwordContainer.appendChild(passwordLabel);
-            passwordContainer.appendChild(passwordInput);
-            const statusLabel = document.createElement("h4");
-            const statusButton = document.createElement("button");
-            statusLabel.innerText = T.mods.shapezipelago.inputBox.notConnected;
-            statusButton.innerText = T.mods.shapezipelago.inputBox.connect;
-            statusButton.classList.add("styledButton", "statusButton");
-            statusButton.addEventListener("click", () => {
-                if (!connection) {
-                    modImpl.settings.player = playerInput.value;
-                    modImpl.settings.address = addressInput.value;
-                    modImpl.settings.port = portInput.valueAsNumber;
-                    modImpl.settings.password = passwordInput.value;
-                    modImpl.saveSettings();
-                    const connectInfo = {
-                        hostname: addressInput.value,
-                        port: portInput.valueAsNumber,
-                        game: "shapez",
-                        name: playerInput.value,
-                        items_handling: ITEMS_HANDLING_FLAGS.REMOTE_ALL,
-                        password: passwordInput.value,
-                    };
-                    new Connection()
-                        .tryConnect(connectInfo, (packet) => processItemsPacket(root, packet))
-                        .finally(function () {
-                            if (connection) {
-                                statusLabel.innerText = T.mods.shapezipelago.inputBox.connected;
-                                statusButton.innerText = T.mods.shapezipelago.inputBox.disconnect;
-                            } else {
-                                statusLabel.innerText = T.mods.shapezipelago.inputBox.failed;
-                            }
-                        });
-                } else {
-                    connection.disconnect();
-                    statusLabel.innerText = T.mods.shapezipelago.inputBox.disconnected;
-                    statusButton.innerText = T.mods.shapezipelago.inputBox.connect;
-                }
-            });
-            statusContainer.appendChild(statusLabel);
-            statusContainer.appendChild(statusButton);
+        if (state.key !== "MainMenuState") {
+            return;
         }
+
+        const menuAddOn = `
+            <div class="inputContainer">
+                <div class="playerContainer">
+                    <h4>${T.mods.shapezipelago.inputBox.player}</h4>
+                    <input id="shapezipelagoPlayerInput" class="playerInput" type="text" value="${modImpl.settings.player}"/>
+                </div>
+                <div class="addressContainer">
+                    <h4>${T.mods.shapezipelago.inputBox.address}</h4>
+                    <input id="shapezipelagoAddressInput" class="addressInput" type="text" value="${modImpl.settings.address}"/>
+                </div>
+                <div class="portContainer">
+                    <h4>${T.mods.shapezipelago.inputBox.port}</h4>
+                    <input id="shapezipelagoPortInput" type="number" value="${modImpl.settings.port}"/>
+                </div>
+                <div class="passwordContainer">
+                    <h4>${T.mods.shapezipelago.inputBox.password}</h4>
+                    <input id="shapezipelagoPasswordInput" type="text" value="${modImpl.settings.password}"/>
+                </div>
+                <div class="statusContainer">
+                    <h4 id="shapezipelagoConnectionStatus">${T.mods.shapezipelago.inputBox.notConnected}</h4>
+                    <button id="shapezipelagoConnectButton" class="styledButton statusButton">${T.mods.shapezipelago.inputBox.connect}</button>
+                </div>
+            </div>
+        `;
+
+        const mainWrapper = document.body.getElementsByClassName("mainWrapper").item(0);
+        const sideContainer = mainWrapper.getElementsByClassName("sideContainer").item(0);
+        sideContainer.insertAdjacentHTML("beforeend", menuAddOn);
+
+        document.getElementById("shapezipelagoConnectButton").addEventListener("click", () => {
+            const statusLabel = document.getElementById("shapezipelagoStatusLabel");
+            const statusButton = document.getElementById("shapezipelagoConnectButton");
+
+            if (connection) {
+                connection.disconnect();
+                statusLabel.innerText = T.mods.shapezipelago.inputBox.disconnected;
+                statusButton.innerText = T.mods.shapezipelago.inputBox.connect;
+                return;
+            }
+
+            const player = document.getElementById("shapezipelagoPlayerInput").value;
+            const address = document.getElementById("shapezipelagoAddressInput").value;
+            const port = document.getElementById("shapezipelagoAddressInput").valueAsNumber;
+            const password = document.getElementById("shapezipelagoAddressInput").value;
+
+            modImpl.settings.player = player;
+            modImpl.settings.address = address;
+            modImpl.settings.port = port;
+            modImpl.settings.password = password;
+            modImpl.saveSettings();
+
+            const connectInfo = {
+                game: "shapez",
+                player,
+                hostname: address,
+                port,
+                password,
+                items_handling: ITEMS_HANDLING_FLAGS.REMOTE_ALL,
+            };
+
+            new Connection()
+                .tryConnect(connectInfo, (packet) => processItemsPacket(root, packet))
+                .finally(() => {
+                    if (connection) {
+                        statusLabel.innerText = T.mods.shapezipelago.inputBox.connected;
+                        statusButton.innerText = T.mods.shapezipelago.inputBox.disconnect;
+                    } else {
+                        statusLabel.innerText = T.mods.shapezipelago.inputBox.failed;
+                    }
+                });
+        });
     });
 }
